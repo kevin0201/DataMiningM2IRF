ffb6cc1cf170edf6949939af291732f523d7f439
kevin <kevincedric.y@gmail.com>
2017-04-09 20:48:36 +0200

    noms des colonnes du tdc et de son contenu
    creation du tableau disjonct complet, avec les noms des intervalles, et
    remplacement de 0,1 par oui,non...

--- datamining.R
+++ datamining.R
@@ -46,7 +46,7 @@ df5 = df5[,-1]
 
 # Observation du boxplot 
 
-boxplot(df5[,2:ncol(df2)], use.cols = T)
+# boxplot(df5[,2:ncol(df2)], use.cols = T)
 
 # for (i in 1:ncol(df5)){
 #   
@@ -78,15 +78,30 @@ interv=matrix(0,nrow = ncol(df5),ncol=nint+1)
 
 for (i in 1:ncol(df5)){
   
-  interv[i,] = classIntervals(df[,i],style = methodeinterv,n=nint)$brks
+  interv[i,] = classIntervals(df5[,i],style = methodeinterv,n=nint)$brks
 
 }
+
+# Creation des noms de la colonne de la matrice disjonctif
+# Les nom sont de la forme [borne inf nom_variable borne supp]
+nomvarint = matrix(0,ncol(df5),ncol(interv)-1)
+
+for(i in 1:ncol(df5)){
+  
+  for(j in 1:ncol(interv)-1){
+    
+    nomvarint[i,j] = paste(interv[i,j],colnames(df5)[i],interv[i,j+1])
+    
+  }
+}
+
+nomvarint = matrix(nomvarint,1,9*23)
 
 # Construction du tableau disjonctif complet
 #  < 5 ">
 # tab_disj_comp = matrix(0,nrow = nrow(df5),ncol = nint)
 
-tab_disj_comp = array(data = 0, dim = c(nrow(df5),nint,ncol(df5)), dimnames = NULL)
+tab_disj_comp = array(data = "Non", dim = c(nrow(df5),nint,ncol(df5)), dimnames = NULL)
 
 for (k in 1:ncol(df5))
 {
@@ -96,7 +111,7 @@ for (k in 1:ncol(df5))
           {
                 if(df5[l,k]>=interv[k,f] && df5[l,k]<interv[k,f+1])
                 {
-                    tab_disj_comp[l,f,k]=1
+                    tab_disj_comp[l,f,k]="Oui"
                 }
           }
     }
@@ -110,13 +125,17 @@ for (z in 2:dim(tab_disj_comp)[3]){
   
 }
 
+# tab1 est le tableau disjoctif complet
+# On change les noms de colonnes du tableau disjonctif complet
+tab1 = `colnames<-`(tab1,nomvarint)
+
 # On sauvegarde le tableau disjonctif creer et la matrice des intervalles
-# write.csv(tab1,"tableau_disjonctif_complet.csv")
-# write.csv(interv,"intervalles_variable.csv")
+ # write.csv(tab1,"tableau_disjonctif_complet.csv")
+ # write.csv(interv,"intervalles_variable.csv")
 
 #On importe le tableau disjonctif
 
-disjonct = read.csv("tableau_disjonctif_csv.csv", header = T,sep = ";")
+# disjonct = read.csv("tableau_disjonctif_complet.csv", header = T,sep = ";")
 
 
 # ACM
@@ -135,9 +154,9 @@ disjonct = read.csv("tableau_disjonctif_csv.csv", header = T,sep = ";")
 library(FactoMineR)
 
 # ACM
-res.mca = MCA(disjonct)
+res.mca = MCA(tab1)
 
-plot.MCA(res.mca,  invisible = "var",cex=0.7)
+#plot.MCA(res.mca,  invisible = "var",cex=0.7)
 plotellipses(res.mca)
 
 #CAH
